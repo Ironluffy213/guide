7414703af98455a557f910ed43390098317e1afa
Sean Kim <51925069+Ironluffy213@users.noreply.github.com>
2022-07-22 18:05:36 -0700

    Censor

--- Bronze/src/USACOCensoring.java
+++ Bronze/src/USACOCensoring.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class USACOCensoring {
 	static Scanner in;
 	static PrintWriter out;
-	static ArrayList<Character> s, t;
+	static String s, t;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		in = new Scanner(new File("censor.in"));
@@ -18,37 +18,21 @@ public class USACOCensoring {
 	}
 	
 	static void init() {
-		s = new ArrayList<Character>();
-		t = new ArrayList<Character>();
-		
-		String temp = in.nextLine();
-		for(int i = 0; i < temp.length(); i++) {
-			s.add(temp.charAt(i));
-		}
-		
-		temp = in.nextLine();
-		for(int i = 0; i < temp.length(); i++) {
-			t.add(temp.charAt(i));
-		}
+		s = in.nextLine();
+		t = in.nextLine();
 	}
 	
 	static void solve() {
-		for(int i = 0; i < s.size()-t.size(); i++) {
-			int same = 0;
-			for(int j = 0; j < t.size(); j++) {
-				if(s.get(i+j) == t.get(j)) {
-					same++;
-				}else {
-					break;
-				}
-			}
-			if(same == t.size()) {
-				for(int j = 0; j < t.size(); j++){
-					s.remove(i+j);
-				}
-				i = 0;
+		StringBuilder sb = new StringBuilder();
+		int len = t.length();
+		
+		for(int i = 0; i < s.length(); i++) {
+			sb.append(s.charAt(i));
+			if(sb.length() >= len && sb.substring(sb.length()-len).equals(t)) {
+				sb.delete(sb.length()-len, sb.length()+1);
 			}
 		}
-		out.println(s);
+		
+		out.println(sb);
 	}
 }
