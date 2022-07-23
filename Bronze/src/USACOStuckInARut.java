abcca16b916bc2a187df8131b1e6eac722e6d468
Sean Kim <51925069+Ironluffy213@users.noreply.github.com>
2022-07-22 17:43:18 -0700

    Update USACOStuckInARut.java

--- Bronze/src/USACOStuckInARut.java
+++ Bronze/src/USACOStuckInARut.java
@@ -21,21 +21,64 @@ public class USACOStuckInARut {
 		
 		for(int i = 0; i < n; i++) {
 			String[] temp = in.nextLine().split(" ");
-			cow[i] = new triple(Integer.parseInt(temp[0]), temp[1], Integer.parseInt(temp[2]));
+			//System.out.println(Arrays.toString(temp));
+			cow[i] = new triple(temp[0].charAt(0), Integer.parseInt(temp[1]), Integer.parseInt(temp[2]));
+			//System.out.println(temp[1]);
 		}
+		//System.out.println(Arrays.toString(cow));
 	}
 	
 	static void solve() {
+		int min = 0;
+		for(int i = 0; i < n; i++) {
+			min = check(i);
+			if(min == Integer.MAX_VALUE) {
+				System.out.println("Infinity");
+			}else {
+				System.out.println(min);
+			}
+		}
+	}
+	
+	static int check(int i) {
+		int min = Integer.MAX_VALUE;
 		
+		for(int j = 0; j < n; j++) {
+			if(i == j || cow[i].dir == cow[j].dir) {
+				continue;
+			}
+			
+			int dx = Math.abs(cow[i].x - cow[j].x);
+			int dy = Math.abs(cow[i].y - cow[j].y);
+			
+			if(cow[i].dir == 'E') {
+				if(cow[i].x > cow[j].x || cow[i].y < cow[j].y) {
+					continue;
+				}
+				
+				if(dx > dy && check(j) >= dx) {
+					min = Math.min(min, dx);
+				}
+			}else {
+				if(cow[i].x < cow[j].x || cow[i].y > cow[j].y) {
+					continue;
+				}
+				
+				if(dx < dy && check(j) >= dy) {
+					min = Math.min(min, dy);
+				}
+			}
+		}
+		return min;
 	}
 	
 	static class triple implements Comparable<triple> {
 		int x, y;
-		String dir;
+		char dir;
 		
-		triple(int a, String s, int b){
-			x = x;
-			y = y;
+		triple(char s, int a, int b){
+			x = a;
+			y = b;
 			dir = s;
 		}
 		
